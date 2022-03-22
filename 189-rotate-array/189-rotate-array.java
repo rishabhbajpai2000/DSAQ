4ffe08129bf4bd9d6cca59269559c4f3305eacf0
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-03-23 00:20:28 +0530

    Time: 2 ms (46.68%), Space: 64.5 MB (42.06%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i = 0; i<nums.length; i++)  {
             copy[i] = nums[i];
         }
+        // now we are copying the elements from "copy" to desired location in nums
         for(int i = 0; i<nums.length; i++)  {
             int new_pos = (i+k)%nums.length;
             nums[new_pos] = copy[i];
