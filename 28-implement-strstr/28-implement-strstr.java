37dd4077190720b95c4e646736695a379fa94b3d
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-04-19 01:28:04 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (98.30%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,13 +1,16 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int l1=haystack.length();//5
-        int l2=needle.length();//3
-        int c=0;
-        for(int i=0;i<l1-l2+1;i++){
-           String sub=haystack.substring(i,i+l2); 
-            if(sub.equals(needle))
+        if (haystack.length() == 0 && needle.length() == 0)
+            return 0;
+        else{
+            for (int i = 0; i<haystack.length() - needle.length()+1;i++){
+            String word = haystack.substring(i,i+needle.length());
+            if (word.equals(needle))
                 return i;
         }
         return -1;
+        }
+        
+        
     }
 }
