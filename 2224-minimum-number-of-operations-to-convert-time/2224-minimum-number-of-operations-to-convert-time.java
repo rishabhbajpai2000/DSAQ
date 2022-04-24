91f46c396880d46c455f6c4f9aa6c79df4f38e2d
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-04-24 21:30:44 +0530

    Time: 3 ms (51.35%), Space: 42.5 MB (48.10%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.java
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.java
@@ -2,7 +2,6 @@ class Solution {
     public int convertTime(String current, String correct) {
         int time_d = convertmin(correct) - convertmin(current);
         int rounds = 0;
-        
         int r = 0;
         while(time_d != 0){
             if (time_d >= 60){
@@ -28,7 +27,7 @@ class Solution {
         }
         return rounds;
     }
-    static int convertmin(String a){
+     int convertmin(String a){
         int ans = 0;
         String[] time = a.split(":");
         ans += Integer.parseInt(time[0])*60 + Integer.parseInt(time[1]);
