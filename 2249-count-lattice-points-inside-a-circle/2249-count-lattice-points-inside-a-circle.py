05c1fb8a55eff94445bcacf07248bf376861565f
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-04-25 23:38:38 +0530

    Time: 6118 ms (33.33%), Space: 18.7 MB (33.33%) - LeetHub

--- 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.py
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.py
@@ -1,25 +1,9 @@
-def check(x, y, arr):
-    xi = arr[0]
-    yi = arr[1]
-    r = arr[2]
-    return (xi-x)*(xi-x) + (yi-y)*(yi-y) <= r*r
-
-def points(arr): # list of co ordinates, that are in circle
-    ans = set()
-    xi = arr[0]
-    yi = arr[1]
-    r = arr[2]
-    for x in range(xi-r, xi+r+1):
-        for y in range(yi-r, yi+r+1):
-            if check(x,y, arr):
-                ans.add((x,y))
-    return ans
-class Solution:  
-    def countLatticePoints(self, circles: List[List[int]]) -> int:
-        ans = set()
-        for arr in circles:
-            c = points(arr)
-            for elem in c:
-                ans.add(elem)
-        return len(ans)
-        
+class Solution:
+    def countLatticePoints(self, A: List[List[int]]) -> int:
+        res = set()
+        for x,y,r in A:
+            for i in range(x - r,x + r + 1):
+                for j in range(y - r, y + r + 1):
+                    if (x - i) ** 2 + (y - j) ** 2 <= r * r:
+                        res.add((i,j))
+        return len(res)
