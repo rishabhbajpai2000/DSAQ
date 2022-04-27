3d7bec76ac9699049e77cdc17f07083bba9d2c53
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-04-27 12:10:23 +0530

    Time: 27 ms (34.57%), Space: 13.5 MB (42.11%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        return matrix and list(matrix.pop(0)) + self.spiralOrder(zip(*matrix)[::-1])
+        
