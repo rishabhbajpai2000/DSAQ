0f3c524e427f0075f496fe6eca76d60e50fac38f
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-06-25 06:01:19 +0530

    Time: 6 ms (5.18%), Space: 39.6 MB (82.60%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        // making a two d array, initially it will be filled with zeroes all along 
+        int[][] arr = new int[m+1][n+1];
+        // last row last column is made 1
+        arr[0][0] = 1;
+        arr[m-1][n-1] = 1;
+        // now we need to traverse the array and fill in the values.
+        for (int i = m-1; i>-1; i--){
+            for (int j = n-1; j>-1; j--){
+                if (i == m-1 && j == n-1) continue;
+                arr[i][j] = arr[i][j+1] + arr[i+1][j];
+            }
+        }
+        
+        // printing out the array to test the results.
+        for(int[] elem: arr)
+            System.out.println(Arrays.toString(elem));
+        
+        return arr[0][0];
+    }
+}
