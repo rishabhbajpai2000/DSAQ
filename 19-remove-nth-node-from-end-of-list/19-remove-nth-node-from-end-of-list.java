ee180925db3cba0b7f25ef2b34129aa21880b13d
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-07-25 11:16:46 +0530

    Time: 1 ms (70.27%), Space: 42.4 MB (34.87%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -13,29 +13,22 @@ class Solution {
         
         // going to the node just before the node which we need to delete. . 
         temp = head;
-        int ind = l-n-1;    
-        int i;        
+        int ind = l-n-1;     
         
-        if(ind>=0)
-        {
-        for ( i = 0; i<ind ;i++)
-            temp = temp.next;
+        if(ind>=0){
+            for (int i = 0; i<ind ;i++)
+                temp = temp.next;
         
-
         if (temp.next != null) 
-        temp.next = temp.next.next;
+            temp.next = temp.next.next;
         
         return head;
         }
-        else
-        {
-              if (temp.next != null)
-            head  = temp.next;
+        else{
+            if (temp.next != null)
+                return temp.next;
             else
                 return null;
-            return head;
-        }
-        
-        
+        }        
     }
 }
