00eab0f4734909a6fbc83b0a769bf7e685a676fc
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-08-25 18:45:38 +0530

    Time: 2 ms (65.72%), Space: 42.5 MB (75.69%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,21 +1,18 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        
         if( digits.equals("")){
             List<String> l = new ArrayList<>();
             return l;
         }
-        list = new ArrayList<>();
-        combinations("", digits);
-        return list;
+        return combinations("",digits);
     }
     
-     static List<String> list = new ArrayList<>();    
-     static void combinations(String pr, String unpr) {
+    List<String> combinations(String pr, String unpr) {
 
         if (unpr.isEmpty()) {
+            List<String> list = new ArrayList<>();
             list.add(pr);
-            return;
+            return list;
         }
         String[][] phone = {
                 { "a", "b", "c" },
@@ -29,9 +26,11 @@ class Solution {
 
         int num = unpr.charAt(0) - '0';
 
+        List<String> list = new ArrayList<>();
         for (int i = 0; i < phone[num - 2].length; i++) {
             String ch =  phone[num-2][i];
-            combinations(pr + ch, unpr.substring(1));
+            list.addAll(combinations(pr + ch, unpr.substring(1)));
         }
-    }
+        return list;
+    } 
 }
