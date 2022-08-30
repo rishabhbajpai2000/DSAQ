015b18ca9698961e9265f73307d808c13449c745
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-08-31 02:16:47 +0530

    Time: 717 ms (22.68%), Space: 42.1 MB (64.63%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -5,13 +5,13 @@ class Solution {
         return ans;
     }
 
-    private static int target(int[] nums, int target, int sum, int i) {
-        if (i == nums.length) {
+    private static int target(int[] nums, int target, int sum, int index) {
+        if (index == nums.length) {
             if (sum == target) 
                 return 1;
             return 0;
         }
 
-        return target(nums, target, sum + nums[i], i+1) + target(nums,target, sum - nums[i], i+1);
+        return target(nums, target, sum + nums[index], index+1) + target(nums,target, sum - nums[index], index+1);
     }
     }
