50febf2ad5f2b7c84633d8be8a29c9d405b99b0b
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-09-04 02:35:03 +0530

    Time: 145 ms (64.24%), Space: 14.1 MB (56.66%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -6,17 +6,8 @@ class Solution:
                 freq[i] += 1
             else:
                 freq[i] = 1
-        probable = []
-        for i in freq:
-            if freq[i] == 1:
-                probable.append(i)
-
-        minm = len(s)
-        for i in probable:
-            for j in range(len(s)):
-                if i == s[j]:
-                    minm = min(j,minm)
-                    break
-        if minm == len(s):
-            return -1 
-        return minm
+                
+        for i in range(len(s)):
+            if freq[s[i]] == 1:
+                return i
+        return -1
