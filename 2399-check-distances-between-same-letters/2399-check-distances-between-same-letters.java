1baf97cafafd47d430906380684b5a86b7118ec7
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-09-05 04:13:22 +0530

    Time: 1 ms (100.00%), Space: 43.5 MB (18.75%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.java
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean checkDistances(String s, int[] distance) {
-        int[] check = new int[26];
-        int[] occurance = new int[26];
+        int[] check = new int[26+26];
+
         // for (int i = 0; i<26; i++) check[i] = -1;
         
         for(int i = 0; i<s.length(); i++){
@@ -9,12 +9,12 @@ class Solution {
             int pos_in_check = c-'a';
             if (check[pos_in_check] == 0){
                 check[pos_in_check] = -(i + 1);                         
-                occurance[pos_in_check] = -1;
+                check[pos_in_check + 26] = -1;
             }
             else check[pos_in_check] += i;
         }
-        for(int i = 0; i<check.length; i++)
-            if (occurance[i] == -1 && check[i] != distance[i]) return false;
+        for(int i = 0; i<26; i++)
+            if (check[i+26] == -1 && check[i] != distance[i]) return false;
         return true;
     }
 }
