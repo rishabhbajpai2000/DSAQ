05a27f4754891ecf564fe5046a80fd245f1a6901
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-09-16 01:47:36 +0530

    Time: 27 ms (5.16%), Space: 46.7 MB (5.45%) - LeetHub

---
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean areNumbersAscending(String s) {;
+        int c = 0;
+        String[] sp = s.split(" ");
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        
+        for(String str: sp){
+            if (isNumeric(str)) nums.add(Integer.parseInt(str)); 
+        }
+        
+        System.out.println(nums);
+        int l = nums.size();
+        for (int i = 0; i < l-1; i++) {
+            if (nums.get(i) >= nums.get(i + 1))
+                return false;
+        }
+        return true;
+    }
+    
+    static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
+}
