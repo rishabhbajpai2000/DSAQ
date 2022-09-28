cb8375f16a2b664ed59ac2b4790db57d007c55d6
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-09-28 19:08:04 +0530

    Time: 1 ms (89.23%), Space: 39.9 MB (98.41%) - LeetHub

--- 2409-count-days-spent-together/2409-count-days-spent-together.java
+++ 2409-count-days-spent-together/2409-count-days-spent-together.java
@@ -1,18 +1,10 @@
 class Solution {
     public int countDaysTogether(String arriveAlice, String leaveAlice, String arriveBob, String leaveBob) {
         
-        
-        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30,31,30,31};
-        int sum = 0;
-        for (int i = 0; i<days.length;i++){
-            sum += days[i];
-            days[i] = sum;
-        }
-
-        int a_ar = date_to_i(arriveAlice, days);
-        int a_l = date_to_i(leaveAlice, days);
-        int b_ar = date_to_i(arriveBob, days);
-        int b_l = date_to_i(leaveBob, days);
+        int a_ar = date_to_i(arriveAlice);
+        int a_l = date_to_i(leaveAlice);
+        int b_ar = date_to_i(arriveBob);
+        int b_l = date_to_i(leaveBob);
         
         int[] year = new int[366];
         for (int i = a_ar; i <= a_l; i++) 
@@ -30,7 +22,13 @@ class Solution {
         return count;
     } 
     
-    int date_to_i(String date_string,int[] days){
+    int date_to_i(String date_string){
+        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30,31,30,31};
+        int sum = 0;
+        for (int i = 0; i<days.length;i++){
+            sum += days[i];
+            days[i] = sum;
+        }
         int month = Integer.parseInt(date_string.substring(0,2));
         int date =  Integer.parseInt(date_string.substring(3,5));
         
