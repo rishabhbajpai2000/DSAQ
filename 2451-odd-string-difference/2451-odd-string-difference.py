f4edf113893eb06965c967b48ef5058a12231b87
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-10-29 22:31:01 +0530

    Time: 41 ms (100.00%), Space: 13.8 MB (66.67%) - LeetHub

--- 2451-odd-string-difference/2451-odd-string-difference.py
+++ 2451-odd-string-difference/2451-odd-string-difference.py
@@ -1,13 +1,13 @@
+def diff_arr(word):
+    diff = []
+    for i in range(len(word)-1):
+        diff.append(ord(word[i+1]) - ord(word[i]))
+    return diff
+
 class Solution:
     def oddString(self, words: List[str]) -> str:
         
         dic = {}
-
-        def diff_arr(word):
-            diff = []
-            for i in range(len(word)-1):
-                diff.append(ord(word[i+1]) - ord(word[i]))
-            return diff
 
         for word in words:
             diff = diff_arr(word)
@@ -17,10 +17,6 @@ class Solution:
             else:
                 a = [word]
                 dic[str(diff)] = a
-        # print(dic)
-        # for i in dic:
-        #     print(i + str(dic[i]))
-
 
         for i in dic:
             if len(dic[i]) == 1:
