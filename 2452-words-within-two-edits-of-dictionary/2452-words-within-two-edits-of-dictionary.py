f987e435df3a0d078532dd1bba68afdc8bdf735e
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-10-29 23:40:35 +0530

    Time: 400 ms (100.00%), Space: 14.2 MB (100.00%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -0,0 +1,21 @@
+def edit(word1, word2):
+    mistakes = 0
+    
+    for i in range(len(word1)):
+        if word1[i] != word2[i]: mistakes+= 1
+            
+    if mistakes<3:
+        return True
+    return False
+
+class Solution:
+    def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
+        answers = []
+        
+        for i in queries:
+            for j in dictionary:
+                if edit(i, j): 
+                    answers.append(i)
+                    break
+        # answers = list(answers)
+        return answers
