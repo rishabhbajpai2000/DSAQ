bdf4fd20542df54f77a0e48b08c190f0bd410c7f
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-10-30 15:43:41 +0530

    Time: 407 ms (100.00%), Space: 18.3 MB (100.00%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -1,21 +1,86 @@
-def edit(word1, word2):
-    mistakes = 0
-    
-    for i in range(len(word1)):
-        if word1[i] != word2[i]: mistakes+= 1
-            
-    if mistakes<3:
-        return True
-    return False
+class TrieNode:
+    def __init__(self, isEOW: bool = False) -> None:
+        self.isEOW: bool = isEOW
+        self.children: Dict[int, "TrieNode"] = {}
 
+class Trie:
+    def __init__(self) -> None:
+        self.root = TrieNode()
+    
+    def insert(self, word: str) -> None:
+        nextNode: "TrieNode"
+        i: int = 0
+        root: "TrieNode" = self.root
+
+        while i < len(word) and ord(word[i]) in root.children:
+            root = root.children[ord(word[i])]
+            i += 1
+
+        while i < len(word):
+            nextNode = TrieNode()
+            root.children[ord(word[i])] = nextNode
+            root = nextNode
+            i += 1
+
+        root.isEOW = True
+        
+    def takeAll(self, l: List[str]) -> None:
+        for i in l:
+            self.insert(i)
+
+    def search(self, word: str) -> bool:
+        root = self.root
+
+        for i in word:
+            if ord(i) not in root.children:
+                return False
+            root = root.children[ord(i)]
+            
+        return root.isEOW
+        
+    def startsWith(self, prefix: str) -> bool:
+        root = self.root
+
+        for i in prefix:
+            if ord(i) not in root.children:
+                return False
+            root = root.children[ord(i)]
+            
+        return True
+    
+    def is_present(self, word) -> bool:
+        def dfs(root, i, c) -> bool:
+            if c >= 3:
+                return False
+            
+            while i < len(word) and ord(word[i]) in root.children:
+                root = root.children[ord(word[i])]
+                i += 1
+                
+            if i >= len(word):
+                return root.isEOW
+
+            return  bool(sum([1 if dfs(j, i + 1, c + 1) else 0 for j in root.children.values()]) > 0)
+        
+        r = []
+        for k, v in self.root.children.items():
+            if k == ord(word[0]): 
+                r.append(1 if dfs(self.root, 0, 0) else 0)
+            else:
+                r.append(1 if dfs(v, 1 , 1) else 0)
+
+        return bool(sum(r) > 0)
+
+    
 class Solution:
     def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
-        answers = []
+        trie = Trie()
+        trie.takeAll(dictionary)
+        
+        ret = []
         
         for i in queries:
-            for j in dictionary:
-                if edit(i, j): 
-                    answers.append(i)
-                    break
-
-        return answers
+            if trie.is_present(i):
+                ret.append(i)
+                
+        return ret
