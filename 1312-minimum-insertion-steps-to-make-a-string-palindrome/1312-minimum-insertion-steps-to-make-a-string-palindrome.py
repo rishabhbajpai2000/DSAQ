889e9cf0b612f2b07f7190097eba4e74d0596469
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-11-12 02:16:02 +0530

    Time: 1827 ms (62.89%), Space: 34.2 MB (17.15%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        
+        def l(s: str) -> int:
+            def longestCommonSubsequence( text1: str, text2: str) -> int:
+                dp = []
+                for i in range(len(text1)):
+                    local = []
+                    for j in range(len(text2)):
+                        local.append(-1)
+                    dp.append(local)
+
+
+                def helper(ind1, ind2):
+                    if ind1 < 0 or ind2 <0: return 0
+                    if dp[ind1][ind2] != -1: return dp[ind1][ind2]
+
+                    if text1[ind1] == text2[ind2]:
+                        return 1 + helper(ind1-1, ind2-1)
+
+                    dp[ind1][ind2] = max(helper(ind1-1, ind2), helper(ind1, ind2-1))
+                    return dp[ind1][ind2]
+
+                return helper(len(text1)-1, len(text2)-1)
+
+
+            text1 = s
+            text2 = s[::-1]
+            return longestCommonSubsequence( text1, text2)
+        
+        return len(s) - l(s)
+    
