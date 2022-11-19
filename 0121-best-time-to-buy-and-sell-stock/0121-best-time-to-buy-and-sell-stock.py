e90df3e22eef4725501ebc28ec1c506aee622f78
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-11-19 14:49:38 +0530

    Time: 2477 ms (51.18%), Space: 25.8 MB (7.28%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # find global minima till now
+        minima = []
+        minima.append(prices[0])
+        
+        for i in range(1,len(prices)):
+            if prices[i]<minima[i-1]:
+                minima.append(prices[i])
+            else:
+                minima.append(minima[i-1])
+        print(minima)
+        global_profit = 0
+        for i in range(len(prices)):
+            profit = prices[i]-minima[i]
+            global_profit = max(profit, global_profit)
+        
+        return global_profit
+        
+        
