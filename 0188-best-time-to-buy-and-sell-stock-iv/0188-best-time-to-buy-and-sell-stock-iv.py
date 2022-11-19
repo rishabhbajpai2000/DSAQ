ad2646ff57a9af48e6e38f9f334bd3090ee1a60d
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-11-20 03:02:55 +0530

    Time: 899 ms (14.49%), Space: 30 MB (22.41%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+                # making a dp array 
+        dp = []
+        cap = k
+        for i in range(len(prices)):
+            first_local = []
+            for j in range(2):
+                local = []
+                for k in range(cap+1):
+                    local.append(-1)
+                first_local.append(local)
+            dp.append(first_local)
+                
+        def helper(ind, buy,cap):
+            if ind== len(prices) or cap == 0:
+                return 0;
+            
+            if dp[ind][buy][cap] != -1: return dp[ind][buy][cap]
+            
+            if buy == 1:
+                dp[ind][buy][cap] = max(-prices[ind] + helper(ind+1, 0, cap), 0 + helper(ind + 1, 1, cap))
+                return dp[ind][buy][cap]
+            dp[ind][buy][cap] =  max(prices[ind] + helper(ind+1, 1, cap-1), 0 + helper(ind+1, 0, cap))
+            return dp[ind][buy][cap]
+        
+        
+        return helper(0, 1, cap)
