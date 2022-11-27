4cbb0b040712fde17296815d62ab4c85dbcfa4a0
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-11-27 16:08:39 +0530

    Time: 37 ms (87.34%), Space: 14 MB (5.88%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,27 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def helper(index, nums, dp):
+        def helper(index):
             if index >= len(nums):
                 return 0
             if dp[index] != -1: return dp[index]
             
-            chosen = nums[index] + helper(index+2, nums, dp)
-            not_chosen = helper(index+1, nums, dp)
+            chosen = nums[index] + helper(index+2)
+            not_chosen = helper(index+1)
             
             dp[index] = max(chosen, not_chosen)
             return dp[index]
         
         if len(nums) == 1: return nums[0]
+        
         last = nums[-1]
         nums = nums[:-1]
         dp = [-1] * len(nums)
-        ans1 = helper(0, nums, dp)
+        ans1 = helper(0)
         
         
         nums.append(last)
         dp = [-1] * len(nums)
         nums = nums[1:]
-        ans2 = helper(0, nums, dp)
+        ans2 = helper(0)
         
         return max(ans1, ans2)
         
