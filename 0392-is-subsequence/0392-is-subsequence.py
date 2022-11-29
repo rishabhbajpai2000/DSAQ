6a01e65bc18880d2de694ffaffeab2bcd36c1694
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-11-29 11:41:42 +0530

    Time: 233 ms (5.14%), Space: 26.4 MB (5.51%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,26 @@
+def longestCommonSubsequence( text1: str, text2: str) -> int:
+    
+    dp = [[-1] * len(text2) for _ in range(len(text1))]
+    def helper(index1, index2):
+        if index1 < 0 or index2 < 0:
+            return 0
+        if dp[index1][index2] != -1: return dp[index1][index2]
+
+        dp[index1][index2] = 0
+        if text1[index1] == text2[index2]: 
+            dp[index1][index2] = 1 + helper(index1-1, index2-1)
+        else:
+            dp[index1][index2] = max(helper(index1-1, index2), helper(index1, index2-1))   
+        return dp[index1][index2]
+
+
+    index1, index2 = len(text1)-1, len(text2)-1
+    return helper(index1, index2)
+import re
+class Solution(object):
+    
+    def isSubsequence(self, s, t):
+        ans = longestCommonSubsequence(s, t)  
+        if ans == len(s):
+            return True
+        return False
