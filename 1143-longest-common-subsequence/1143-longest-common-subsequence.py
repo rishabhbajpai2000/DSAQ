93ede82c7f5bb6d65bed2c99127d604e036a9d26
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-11-29 09:42:46 +0530

    Time: 649 ms (77.77%), Space: 24.1 MB (32.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,21 +1,19 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = []
-        for i in range(len(text1)):
-            local = []
-            for j in range(len(text2)):
-                local.append(-1)
-            dp.append(local)
-            
-            
-        def helper(ind1, ind2):
-            if ind1 < 0 or ind2 <0: return 0
-            if dp[ind1][ind2] != -1: return dp[ind1][ind2]
-            
-            if text1[ind1] == text2[ind2]:
-                return 1 + helper(ind1-1, ind2-1)
-            
-            dp[ind1][ind2] = max(helper(ind1-1, ind2), helper(ind1, ind2-1))
-            return dp[ind1][ind2]
         
-        return helper(len(text1)-1, len(text2)-1)
+        dp = [[-1] * len(text2) for _ in range(len(text1))]
+        def helper(index1, index2):
+            if index1 < 0 or index2 < 0:
+                return 0
+            if dp[index1][index2] != -1: return dp[index1][index2]
+            
+            dp[index1][index2] = 0
+            if text1[index1] == text2[index2]: 
+                dp[index1][index2] = 1 + helper(index1-1, index2-1)
+            else:
+                dp[index1][index2] = max(helper(index1-1, index2), helper(index1, index2-1))   
+            return dp[index1][index2]
+        
+        
+        index1, index2 = len(text1)-1, len(text2)-1
+        return helper(index1, index2)
