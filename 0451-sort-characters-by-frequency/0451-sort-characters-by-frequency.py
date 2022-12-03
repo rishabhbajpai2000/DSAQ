ee39c8c536a748c7ab8ea20bfa3df8ccd65d20e3
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2022-12-04 03:23:05 +0530

    Time: 107 ms (38.99%), Space: 33 MB (5.13%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,20 @@
+from collections import OrderedDict
+import numpy as np
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        freq = {}
+        for i in s:
+            if freq.__contains__(i):
+                freq[i] += 1
+            else: 
+                freq[i] = 1
+        print(freq)
+        sorted_dict = dict(sorted(freq.items(), key=lambda item: item[1]))
+
+        ans = ""
+        print(sorted_dict)
+        for i in sorted_dict:
+            ans += i*freq[i]
+        ans = ans[::-1]
+        return ans
+        
