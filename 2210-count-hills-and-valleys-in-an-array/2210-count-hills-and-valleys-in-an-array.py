97233f9b2ba16f9ebc8df94991386ffef2f8aaec
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2023-02-17 23:20:11 +0530

    Time: 51 ms (16.51%), Space: 14.1 MB (14.22%) - LeetHub

---
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
@@ -0,0 +1,27 @@
+def left(index, nums):
+    for i in range(index, -1, -1):
+        if nums[i] != nums[index]:
+            return nums[i]
+    return -1
+
+def right(index, nums):
+    for i in range(index, len(nums)):
+        if nums[i] != nums[index]:
+            return nums[i]
+    return -1
+
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(1, len(nums)-1):
+            left_n = left(i, nums)
+            right_n = right(i, nums)
+            if nums[i-1] == nums[i]: count += 0
+            elif left_n == -1 or right_n == -1: count += 0
+            elif left_n < nums[i] and right_n<nums[i]: 
+                count += 1
+                print(nums[i])
+            elif left_n > nums[i] and right_n > nums[i]: 
+                count += 1
+                print(nums[i])
+        return count
