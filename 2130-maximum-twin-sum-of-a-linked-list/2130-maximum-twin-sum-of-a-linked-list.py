4f1217866db861017f0a6af4202f96a7d0a6e1d0
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2023-05-17 21:50:50 +0530

    Time: 1018 ms (16.68%), Space: 58.4 MB (5.20%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -13,18 +13,16 @@ class Solution:
             l += 1
             dummy = dummy.next
         maxsum = 0
-        dummy = head
         cur_nod_ind = 1
         stack = []
-        while dummy != None:
+        while head != None:
             if cur_nod_ind < l//2 + 1:
-                stack.append(dummy.val)
+                stack.append(head.val)
             else:
-                maxsum = max(stack.pop()+ dummy.val, maxsum)
-                # print(stack.pop())
+                maxsum = max(stack.pop()+ head.val, maxsum)
                 
             cur_nod_ind += 1
-            dummy = dummy.next
+            head = head.next
                 
 
         return maxsum
