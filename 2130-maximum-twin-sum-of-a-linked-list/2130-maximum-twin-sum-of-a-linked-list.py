aedd9d098600c4a1396f623789e7b8fb72196a3a
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2023-05-17 21:47:17 +0530

    Time: 1160 ms (5.09%), Space: 57.5 MB (7.95%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        
+        # counting the length of the list first. 
+        l = 1
+        dummy = head
+        while dummy.next!=None:
+            l += 1
+            dummy = dummy.next
+        maxsum = 0
+        dummy = head
+        cur_nod_ind = 1
+        stack = []
+        while dummy != None:
+            if cur_nod_ind < l//2 + 1:
+                stack.append(dummy.val)
+                dummy = dummy.next
+                # print(cur_nod_ind, stack)
+                cur_nod_ind += 1
+            else:
+                cursum = stack[-1] + dummy.val
+                maxsum = max(cursum, maxsum)
+                stack.pop()
+                dummy = dummy.next
+                # print(cur_nod_ind, stack)
+        return maxsum
