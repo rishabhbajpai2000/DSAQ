0f82b1173d27f14e904b1e4cbd3c51135f16700f
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2023-05-21 16:10:30 +0530

    Time: 64 ms (28.57%), Space: 16.3 MB (57.14%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
@@ -1,6 +1,14 @@
 class Solution:
     def minLength(self, s: str) -> int:
-        while s.count("AB") >  0 or s.count("CD") > 0:
-            first = max(s.find("AB"), s.find("CD"))
-            s = s[:first] + s[first+2:]
-        return len(s)
+        sb = list(s)
+        i = 0
+        while i < len(sb) - 1:
+            if sb[i] == 'A' and sb[i+1] == 'B':
+                del sb[i:i+2]
+                i = max(0, i-1)
+            elif sb[i] == 'C' and sb[i+1] == 'D':
+                del sb[i:i+2]
+                i = max(0, i-1)
+            else:
+                i += 1
+        return len(sb)
